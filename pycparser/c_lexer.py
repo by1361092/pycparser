d370a7fff86d34b8c4e0a16a6a84cde4043edd46
by1361092 <by1361092@outlook.com>
2026-09-16 17:54:46 +0800

    Update c_lexer.py

--- pycparser/c_lexer.py
+++ pycparser/c_lexer.py
@@ -4,7 +4,7 @@
 # CLexer class: lexer for the C language
 #
 # Eli Bendersky [https://eli.thegreenplace.net/]
-# License: BSD
+# SPDX-License-Identifier: BSD-3-Clause
 # ------------------------------------------------------------------------------
 import re
 from collections.abc import Callable
